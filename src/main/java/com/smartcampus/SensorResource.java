6756550ab01fc74fa911abb6c8c587913904f881
Imran Maiwandi <imranmaiwandi@gmail.com>
2026-04-09 21:00:40 +0100

    Implemented SensorResource with GET, POST, and GET by ID endpoints.

--- src/main/java/com/smartcampus/SensorResource.java
+++ src/main/java/com/smartcampus/SensorResource.java
@@ -4,10 +4,44 @@
  */
 package com.smartcampus;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
 /**
  *
  * @author imran
  */
+@Path("/sensors")
 public class SensorResource {
-    
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Sensor> getAllSensors() {
+        return MockDatabase.SENSORS;
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createSensor(Sensor sensor) {
+        if (sensor == null || sensor.getId() == null) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Invalid sensor data").build();
+        }
+        MockDatabase.SENSORS.add(sensor);
+        return Response.status(Response.Status.CREATED).entity(sensor).build();
+    }
+
+    @GET
+    @Path("/{sensorId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getSensorById(@PathParam("sensorId") String sensorId) {
+        for (Sensor sensor : MockDatabase.SENSORS) {
+            if (sensor.getId().equals(sensorId)) {
+                return Response.ok(sensor).build();
+            }
+        }
+        return Response.status(Response.Status.NOT_FOUND).entity("Sensor not found").build();
+    }
 }
