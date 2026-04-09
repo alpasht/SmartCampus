ed76c48727e662e44dfaf9a6eb4e34fdfc0e6a54
Imran Maiwandi <imranmaiwandi@gmail.com>
2026-04-09 20:56:22 +0100

    Implemented `DiscoveryResource` to provide API metadata at `GET /api/v1`.

--- src/main/java/com/smartcampus/DiscoveryResource.java
+++ src/main/java/com/smartcampus/DiscoveryResource.java
@@ -4,10 +4,32 @@
  */
 package com.smartcampus;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author imran
  */
+@Path("/")
 public class DiscoveryResource {
-    
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, Object> getDiscovery() {
+        Map<String, Object> discovery = new HashMap<>();
+        discovery.put("version", "1.0-SNAPSHOT");
+        discovery.put("admin", "admin@smartcampus.com");
+        
+        Map<String, String> resources = new HashMap<>();
+        resources.put("rooms", "/api/v1/rooms");
+        resources.put("sensors", "/api/v1/sensors");
+        
+        discovery.put("resources", resources);
+        return discovery;
+    }
 }
