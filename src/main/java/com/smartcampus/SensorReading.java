d5d87ecb18d6f6c256141e687eeb29d04aa26447
Imran Maiwandi <imranmaiwandi@gmail.com>
2026-04-09 20:55:14 +0100

    Added default constructors, getters, and setters to `Room`, `Sensor`, and `SensorReading` to enable Jackson JSON serialization. Updated .gitignore.

--- src/main/java/com/smartcampus/SensorReading.java
+++ src/main/java/com/smartcampus/SensorReading.java
@@ -13,10 +13,36 @@ public class SensorReading{
     private long timestamp ; // Epoch time (ms) when the reading was captured
     private double value; // The actual metric value recorded bythe hardware
 
+    public SensorReading() {
+    }
+
     public SensorReading(String id, long timestamp, double value){
-    this.id=id;
-    this.timestamp=timestamp;
-    this.value=value;
-}
-    
+        this.id=id;
+        this.timestamp=timestamp;
+        this.value=value;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
 }
