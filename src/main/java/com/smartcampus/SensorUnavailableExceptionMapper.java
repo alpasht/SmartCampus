dea2d8c56f0895772c6d3935840610526193bfc5
Imran Maiwandi <imranmaiwandi@gmail.com>
2026-04-22 11:52:03 +0100

    Implemented mapper for unavailable sensors.

---
+++ src/main/java/com/smartcampus/SensorUnavailableExceptionMapper.java
@@ -0,0 +1,16 @@
+package com.smartcampus;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class SensorUnavailableExceptionMapper implements ExceptionMapper<SensorUnavailableException> {
+    @Override
+    public Response toResponse(SensorUnavailableException exception) {
+        return Response.status(Response.Status.FORBIDDEN)
+                .entity(new ErrorResponse(exception.getMessage()))
+                .type("application/json")
+                .build();
+    }
+}
