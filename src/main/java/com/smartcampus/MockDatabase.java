b55dfc0fdd46f0f00eaae0e163fe47141215a2bd
Imran Maiwandi <imranmaiwandi@gmail.com>
2026-04-22 11:44:22 +0100

    Updated SensorReading to use a concurrent HashMap and updated variable name.

--- src/main/java/com/smartcampus/MockDatabase.java
+++ src/main/java/com/smartcampus/MockDatabase.java
@@ -5,12 +5,14 @@ import com.smartcampus.Sensor;
 import com.smartcampus.SensorReading;
 
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MockDatabase {
     public static final List<Room> ROOMS = new CopyOnWriteArrayList<>();
     public static final List<Sensor> SENSORS = new CopyOnWriteArrayList<>();
-    public static final List<SensorReading> MODULES = new CopyOnWriteArrayList<>();
+    public static final Map<String, List<SensorReading>> READINGS = new ConcurrentHashMap<>();
 
     static {
         // Initialise Rooms
@@ -22,7 +24,10 @@ public class MockDatabase {
         SENSORS.add(new Sensor("TEMP-002","Temperature","ACTIVE",17, "DORM-303"));
 
         // Initialise SensorReadings
-        MODULES.add(new SensorReading("6e19c698-3e92-4981-92e5-9539371d0584", 532, 20));
-        MODULES.add(new SensorReading("2da2c2e4-1e87-40fa-b865-357bde9559d6", 489, 19));
+        READINGS.put("TEMP-001", new CopyOnWriteArrayList<>());
+        READINGS.get("TEMP-001").add(new SensorReading("6e19c698-3e92-4981-92e5-9539371d0584", 1619000000000L, 20));
+        
+        READINGS.put("TEMP-002", new CopyOnWriteArrayList<>());
+        READINGS.get("TEMP-002").add(new SensorReading("2da2c2e4-1e87-40fa-b865-357bde9559d6", 1619000000000L, 19));
     }
 }
