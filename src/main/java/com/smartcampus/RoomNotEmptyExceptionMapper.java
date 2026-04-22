7500601c0270349ee7474ff6e4f016f2b859a6d6
Imran Maiwandi <imranmaiwandi@gmail.com>
2026-04-22 11:51:50 +0100

    Implemented mapper for non-empty rooms.

---
+++ src/main/java/com/smartcampus/RoomNotEmptyExceptionMapper.java
@@ -0,0 +1,16 @@
+package com.smartcampus;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class RoomNotEmptyExceptionMapper implements ExceptionMapper<RoomNotEmptyException> {
+    @Override
+    public Response toResponse(RoomNotEmptyException exception) {
+        return Response.status(Response.Status.CONFLICT)
+                .entity(new ErrorResponse(exception.getMessage()))
+                .type("application/json")
+                .build();
+    }
+}
